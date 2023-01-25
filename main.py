1e3b5b41750233395ea80290d1a5bbba5f4caf68
ekaterinatimoshenko <Kate7t@gmail.com>
2023-01-25 14:48:16 -0500

    add chrome options

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 driver.get("https://www.google.com")
 element = driver.find_element(By.XPATH, "//input[@name='q']")
 element.send_keys("Python")
